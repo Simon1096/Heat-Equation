a612f7407b5e63b4fe622d8d860bc23059a8a266
Simon1096 <s.v.overeem@gmail.com>
2018-02-11 11:23:41 +0100

    First commit

---
+++ Heat-Equation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Heat-Equation
+//
+//  Created by simon van overeem on 11-02-18.
+//  Copyright © 2018 simon van overeem. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
