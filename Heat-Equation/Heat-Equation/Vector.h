dbcaf72b7cb5f258b2bc734e1b11f8669beff423
Jason Xie <elitecqc@gmail.com>
2018-02-13 18:10:01 +0100

    Fully implemented(unless something is missing?) vector class as well as the tests

--- Heat-Equation/Heat-Equation/Vector.h
+++ Heat-Equation/Heat-Equation/Vector.h
@@ -1,1 +1,90 @@
 #pragma once
+
+#include <array>
+#include <memory>
+#include <stdexcept>
+#include <ostream>
+
+using namespace std;
+
+template<typename T>
+class Vector {
+public:
+	int length;
+	T* data;
+
+	Vector() {
+		length = 0;
+		data = new T[0];
+	}
+
+	Vector(int l) {
+		length = l;
+		data = new T[length];
+	}
+
+	Vector(const initializer_list<T> list) : Vector((int)list.size()) {
+		uninitialized_copy(list.begin(), list.end(), data);
+	}
+
+	Vector(const Vector<T> &other) {
+		length = other.length;
+		data = other.data;
+	}
+
+	Vector& operator=(const Vector v) {
+		if (this != &c) {
+			delete[] data;
+			data = new T[v.length];
+			length = v.length;
+
+			copy(v.data, v.data + length, data);
+		}
+		return *this;
+	}
+
+	Vector operator+(const Vector<T> &v) {
+		if (length != v.length)
+			throw invalid_argument("Unequal vector lengths");
+		Vector<T> out(length);
+		for (int i = 0; i < length; i++)
+			out.data[i] = data[i] + v.data[i];
+		return out;
+	}
+
+	Vector operator-(const Vector<T> &v) {
+		if (length != v.length)
+			throw invalid_argument("Unequal vector lengths");
+		Vector<T> out(length);
+		for (int i = 0; i < length; i++)
+			out.data[i] = data[i] - v.data[i];
+		return out;
+	}
+
+	template<typename U>
+	auto operator*(U scalar) -> Vector<decltype(scalar * data[0])> {
+		auto type = scalar * data[0];
+		Vector<decltype(type)> out(length);
+		for (int i = 0; i < length; i++)
+			out.data[i] = scalar * data[i];
+		return out;
+	}
+};
+template<typename T>
+T dot(const Vector<T> &l, const Vector<T> &r) {
+	if (l.length != r.length)
+		throw invalid_argument("Unequal vector lengths");
+	T out = 0;
+	for (int i = 0; i < l.length; i++)
+		out += l.data[i] * r.data[i];
+	return out;
+}
+
+template<typename U, typename T>
+inline auto operator*(U scalar, Vector<T> &v) -> Vector<decltype(scalar * v.data[0])> {
+	auto type = scalar * v.data[0];
+	Vector<decltype(type)> out(v.length);
+	for (int i = 0; i < v.length; i++)
+		out.data[i] = scalar * v.data[i];
+	return out;
+}
--- Heat-Equation/Heat-Equation/main.cpp
+++ Heat-Equation/Heat-Equation/main.cpp
@@ -1,11 +1,51 @@
+#define _SCL_SECURE_NO_WARNINGS
 
 #include <iostream>
+#include "Vector.h"
 
 using namespace std;
 
+template<typename T>
+ostream &operator<<(ostream &os, Vector<T> &v) {
+	if (v.length == 0)
+		return os << "[Empty vector]";
+	os << "[";
+	for (int i = 0; i < v.length; i++)
+		os << v.data[i] << ", ";
+	return os << "\b\b]";
+}
+
 int main() {
-	cout << "Abc" << endl;
+	Vector<int> v;
+	cout << "Default constructor\n" << v << "\n" << endl;
+
+	Vector<int> v2(5);
+	cout << "Constructor with length\n" << v2 << "\n" << endl;
+
+	Vector<int> v3({ 1,2,3,4 });
+	cout << "Constructor with list\n" << v3 << "\n" << endl;
+
+	Vector<int> v4 = { 1,2,3,4 };
+	cout << "Constructor with list 2\n" << v4 << "\n" << endl;
+
+	Vector<int> v5(v2);
+	cout << "Constructor with vector\n" << v5 << "\n" << endl;
+
+	Vector<int> v6 = v4;
+	cout << "Constructor with vector 2\n" << v6 << "\n" << endl;
+
+	cout << "Dot product:\n" << dot(v3, v4) << endl;
+
+	cout << "Addition\n" << (v3 + v4) << "\n" << endl;
+
+	cout << "Subtraction\n" << (v3 - v4) << "\n" << endl;
+
+	cout << "Right hand multiplication\n" << (v3 * 3) << endl;
+	cout << "Right hand multiplication with double\n" << (v3 * 3.5) << "\n" << endl;
+
+	cout << "Left hand multiplication\n" << (3 * v3) << endl;
+	cout << "Left hand multiplication with double\n" << (3.5 * v3) << "\n" << endl;
 
 	system("pause");
-    return 0;
+	return 0;
 }
